f0faffd42c1423f4b6d0c733b91be390f8659a5c
hazemsamyy <hazemsamyy@gmail.com>
2021-04-11 14:49:18 +0200

    added text is url validation

--- src/client/js/validateForm.js
+++ src/client/js/validateForm.js
@@ -1,6 +1,21 @@
 function validateForm(inputText) {
+  function validURL(inputText) {
+    var pattern = new RegExp(
+      '^(https?:\\/\\/)?' +
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    );
+    return !!pattern.test(inputText);
+  }
+
   if (inputText == '') {
     alert('Text must be filled out');
+  } else if ((inputText = validURL(inputText))) {
+    alert('Entered text is a url');
   }
 }
 
